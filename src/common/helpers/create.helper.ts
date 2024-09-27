2cca6ae1fc1af8d94cf2bdc40110cce78e5c996a
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-09-27 17:56:44 -0600

    creacion de metodo para realizar registros dentor de una transaccion

--- src/common/helpers/create.helper.ts
+++ src/common/helpers/create.helper.ts
@@ -7,3 +7,13 @@ export async function createRegister <T>(
   const register = repository.create(data);
   return await repository.save(register);
 }
+
+export async function createRegisterForTransaction(
+  data,
+  manager,
+  entity
+) {
+  const repo = manager.getRepository(entity);
+  await repo.create(data);
+  return repo.save(data); 
+}
