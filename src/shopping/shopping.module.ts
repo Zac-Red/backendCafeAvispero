80571ab699cbab268d2287bf185e7dcbd5244a88
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-09-27 23:28:36 -0600

    se creo y modifico el shopping.module - (se importaron los modulos de InventoryrawmaterialModule, SuppliersModule, RawmaterialModule y ShoppingdetailModule) para usar sus servicios en la transaccion de compra

--- src/shopping/shopping.module.ts
+++ src/shopping/shopping.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ShoppingService } from './shopping.service';
 import { ShoppingController } from './shopping.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shopping } from './entities/shopping.entity';
+import { InventoryrawmaterialModule } from 'src/inventoryrawmaterial/inventoryrawmaterial.module';
+import { SuppliersModule } from 'src/suppliers/suppliers.module';
+import { RawmaterialModule } from 'src/rawmaterial/rawmaterial.module';
+import { ShoppingdetailModule } from 'src/shoppingdetail/shoppingdetail.module';
+import { HandleDBErrors, UuidAdapter } from 'src/common/adapters';
 
 @Module({
   controllers: [ShoppingController],
-  providers: [ShoppingService],
+  providers: [ShoppingService, HandleDBErrors, UuidAdapter],
+  imports: [TypeOrmModule.forFeature([Shopping]), InventoryrawmaterialModule, 
+  SuppliersModule, RawmaterialModule, ShoppingdetailModule],
+  exports: [TypeOrmModule]
 })
 export class ShoppingModule {}
