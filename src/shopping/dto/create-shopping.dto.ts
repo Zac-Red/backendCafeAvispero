d3acc5a99878b18106aaf1c8acccaf83ac56bfe5
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-09-27 18:26:20 -0600

    creacion del create-shoping.dto

--- src/shopping/dto/create-shopping.dto.ts
+++ src/shopping/dto/create-shopping.dto.ts
@@ -1,1 +1,20 @@
-export class CreateShoppingDto {}
+import { IsArray, IsNumber, IsPositive, IsString, IsUUID, MinLength } from "class-validator";
+import { FormatShoppingdetailDto } from "src/shoppingdetail/dto/format-shoppingdetail.dto";
+
+export class CreateShoppingDto {
+  @IsNumber()
+  @IsPositive()
+  total:number;
+  
+  @IsString()
+  @MinLength(1)
+  commercialdocument: string
+
+  @IsString()
+  @MinLength(1)
+  @IsUUID()
+  supplierId:string
+
+  @IsArray()
+  shoppingdetail: FormatShoppingdetailDto[]
+}
