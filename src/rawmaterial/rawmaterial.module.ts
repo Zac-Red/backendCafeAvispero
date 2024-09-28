da31a4005d5b1966638f11241b5feb552f90d7c6
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-09-27 18:20:44 -0600

    modificacion en module rawmaterial.module (se agrego importacion de SuppliersModule, UnitmeasureModule y se exporto el RawmaterialService)

--- src/rawmaterial/rawmaterial.module.ts
+++ src/rawmaterial/rawmaterial.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Rawmaterial } from './entities/rawmaterial.entity';
 import { HandleDBErrors, UuidAdapter } from 'src/common/adapters';
 import { SuppliersModule } from 'src/suppliers/suppliers.module';
+import { UnitmeasureModule } from 'src/unitmeasure/unitmeasure.module';
 
 @Module({
   controllers: [RawmaterialController],
   providers: [RawmaterialService, HandleDBErrors, UuidAdapter],
-  imports: [TypeOrmModule.forFeature([Rawmaterial]), SuppliersModule],
-  exports:[TypeOrmModule]
+  imports: [TypeOrmModule.forFeature([Rawmaterial]), SuppliersModule, UnitmeasureModule],
+  exports:[TypeOrmModule, RawmaterialService]
 })
 export class RawmaterialModule {}
