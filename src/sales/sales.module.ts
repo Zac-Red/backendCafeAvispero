64039030b73821841b70868bbe786a82bd73bb9d
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-10 21:08:26 -0600

    se imporatron los modulos de inventario, cliente, producto y el modulo de detalles de venta

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sale } from './entities/sale.entity';
+import { SalesdetailModule } from 'src/salesdetail/salesdetail.module';
+import { CustomersModule } from 'src/customers/customers.module';
+import { ProductsModule } from 'src/products/products.module';
+import { InventoryproductModule } from 'src/inventoryproduct/inventoryproduct.module';
+import { HandleDBErrors, UuidAdapter } from 'src/common/adapters';
 
 @Module({
   controllers: [SalesController],
-  providers: [SalesService],
+  providers: [SalesService, HandleDBErrors, UuidAdapter],
+  imports: [TypeOrmModule.forFeature([Sale]), forwardRef(() =>SalesdetailModule), 
+  CustomersModule, ProductsModule, InventoryproductModule],
+  exports: [TypeOrmModule, SalesService]
 })
 export class SalesModule {}
