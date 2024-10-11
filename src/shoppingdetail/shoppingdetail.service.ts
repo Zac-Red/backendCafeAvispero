4c34e245f53207552ccc22bf8def0ade4e963408
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-10 21:44:04 -0600

    se agrego el metodo de busqueda de una compra y sus detalles

--- src/shoppingdetail/shoppingdetail.service.ts
+++ src/shoppingdetail/shoppingdetail.service.ts
@@ -1,16 +1,20 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, forwardRef } from '@nestjs/common';
 import { CreateShoppingdetailDto } from './dto/create-shoppingdetail.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ShoppingDetail } from './entities/shoppingdetail.entity';
 import { HandleDBErrors, UuidAdapter } from 'src/common/adapters';
 import { createRegisterForTransaction } from 'src/common/helpers/create.helper';
 import { EntityManager, Repository } from 'typeorm';
+import { ShoppingService } from 'src/shopping/shopping.service';
+import { Shopping } from 'src/shopping/entities/shopping.entity';
 
 @Injectable()
 export class ShoppingdetailService {
   constructor(
     @InjectRepository(ShoppingDetail)
     private readonly shoppingdetailRepository: Repository<ShoppingDetail>,
+    @Inject(forwardRef(() => ShoppingService))
+    private readonly shoppingservice: ShoppingService,
     private readonly DBErrors: HandleDBErrors,
     private readonly uuidAdapter: UuidAdapter, 
   ){}
@@ -25,15 +29,16 @@ export class ShoppingdetailService {
 
   async findOne(term: string) {
     let shoppingdetails: ShoppingDetail[];
+    let shopping: Shopping;
     if(this.uuidAdapter.IsUUID(term)){
+      shopping = await this.shoppingservice.findOne(term);
       const queryBuilder = this.shoppingdetailRepository.createQueryBuilder('shoppingdetail')
-      .leftJoinAndSelect("shoppingdetail.shoppingId", "shopping")
       .leftJoinAndSelect("shoppingdetail.rawmaterialId", "rawmaterial")
       .leftJoinAndSelect("rawmaterial.unitmeasureId", "unitmeasure")
-      shoppingdetails = await queryBuilder.where("shopping.id =:shoppingId", {shoppingId: term}).getMany();    
+      shoppingdetails = await queryBuilder.where("shoppingdetail.shoppingId =:shoppingId", {shoppingId: term}).getMany();    
     }
     if (!shoppingdetails) throw new BadRequestException(`No existe detalles con el ID de compra  ${term}`);
-    return {shoppingdetails};
+    return {shopping, shoppingdetails};
   }
 
   remove(id: number) {
