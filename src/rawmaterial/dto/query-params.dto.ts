89b57e3b21ad6534d1f361755963a6b5c563dec9
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-23 23:28:36 -0600

    se agrego el dto para los queryparams del reporte de materiaprima mas comprada

--- src/rawmaterial/dto/query-params.dto.ts
+++ src/rawmaterial/dto/query-params.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsOptional, IsPositive, IsString } from "class-validator";
+import { IsOptional, IsPositive, IsString, Matches } from "class-validator";
 import { PaginationDto } from "src/common/dtos/pagination.dto";
 
 export class QueryParamsRawMaterials extends PaginationDto{
@@ -24,4 +24,15 @@ export class QueryParamsRawMaterials extends PaginationDto{
   @IsOptional()
   @IsString()
   supplier?:string
+}
+
+
+export class QueryParamsReportTopRawmaterialShoppDto {
+  @IsString()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'La fecha (startOfCurrentMonth) de inicio debe estar en formato YYYY-MM-DD' })
+  startOfCurrentMonth: string;
+  
+  @IsString()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'La fecha (endOfCurrentMonth) fin debe estar en formato YYYY-MM-DD' })
+  endOfCurrentMonth:string;
 }
